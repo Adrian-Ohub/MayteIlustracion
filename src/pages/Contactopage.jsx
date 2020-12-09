af293aed48ff63f6006d0c272db82690f619a1f7
Adrian-Ohub <63118713+Adrian-Ohub@users.noreply.github.com>
2020-12-09 11:05:35 +0100

    Funcionando todo, punto de partida

--- src/pages/Contactopage.jsx
+++ src/pages/Contactopage.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import AppFrame from "../components/AppFrame";
+import { Typography } from "@material-ui/core";
 
 const Contactopage = () => {
-  return <AppFrame></AppFrame>;
+  return (
+    <AppFrame>
+      <Typography variant="h2" color="inherit">
+        Pagina de Contacto (en desarrollo)
+      </Typography>
+    </AppFrame>
+  );
 };
 
 export default Contactopage;
--- src/pages/Ilustracionpage.jsx
+++ src/pages/Ilustracionpage.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppFrame from "../components/AppFrame/AppFrame";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
+import GalleryImgs from "../components/GalleryImgs";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    overflow: "hidden",
+    minHeight: "100%",
+  },
+}));
 
 const Ilustracionpage = () => {
-  return <AppFrame></AppFrame>;
+  /* //funcion de filtro
+  const [keywordList, setKeywordList] = useState("");
+  const myCallBack = (value) => {
+    setKeywordList(value);
+  }; */
+
+  const classes = useStyles();
+
+  return (
+    <AppFrame /* keyword={myCallBack} */>
+      <Grid container item className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h2" color="inherit">
+            Bienvenidos
+          </Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <GalleryImgs keyword={"ilustracion"}></GalleryImgs>
+        </Grid>
+      </Grid>
+    </AppFrame>
+  );
 };
 
 export default Ilustracionpage;
